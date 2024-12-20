6e044009b6d96267fd8db4c12fb8d28c61d4d64c
hemlata1207 <sawariyahemlata41@gmail.com>
2024-12-20 16:32:47 +0530

    created user interface

--- lungcancer.py
+++ lungcancer.py
@@ -32,3 +32,49 @@ global X, Y
 global X_train, X_test, y_train, y_test
 global pca
 
+
+
+font = ('times', 14, 'bold')
+title = Label(main, text='Prediction of time-to-event outcomes in diagnosing lung cancer based on  SVM and compare the accuracy of predicted outcome with Deep CNN algorithm')
+title.config(bg='deep sky blue', fg='white')  
+title.config(font=font)           
+title.config(height=3, width=120)       
+title.place(x=0,y=5)
+
+font1 = ('times', 12, 'bold')
+text=Text(main,height=20,width=150)
+scroll=Scrollbar(text)
+text.configure(yscrollcommand=scroll.set)
+text.place(x=50,y=120)
+text.config(font=font1)
+
+
+font1 = ('times', 13, 'bold')
+uploadButton = Button(main, text="Upload Lung Cancer Dataset", command=uploadDataset)
+uploadButton.place(x=50,y=550)
+uploadButton.config(font=font1)  
+
+readButton = Button(main, text="Read & Split Dataset to Train & Test", command=splitDataset)
+readButton.place(x=350,y=550)
+readButton.config(font=font1) 
+
+svmButton = Button(main, text="Execute SVM Accuracy Algorithms", command=executeSVM)
+svmButton.place(x=50,y=600)
+svmButton.config(font=font1) 
+
+kmeansButton = Button(main, text="Execute CNN Accuracy Algorithm", command=executeCNN)
+kmeansButton.place(x=350,y=600)
+kmeansButton.config(font=font1) 
+
+predictButton = Button(main, text="Predict Lung Cancer", command=predictCancer)
+predictButton.place(x=50,y=650)
+predictButton.config(font=font1)
+
+graphButton = Button(main, text="Accuracy Graph", command=graph)
+graphButton.place(x=350,y=650)
+graphButton.config(font=font1) 
+
+main.config(bg='LightSteelBlue3')
+main.mainloop()
+SVM_CNN_Accuracy.py
+Displaying SVM_CNN_Accuracy.py.
