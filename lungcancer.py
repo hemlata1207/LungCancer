1b8d9f7010d90749f577ce27bc4ea3f914ea95b9
hemlata1207 <sawariyahemlata41@gmail.com>
2024-12-20 16:26:20 +0530

    started new project

---
+++ lungcancer.py
@@ -0,0 +1,23 @@
+
+from tkinter import messagebox
+from tkinter import *
+from tkinter import simpledialog
+import tkinter
+from tkinter import filedialog
+import matplotlib.pyplot as plt
+import numpy as np
+from tkinter.filedialog import askopenfilename
+import pandas as pd 
+import os
+import cv2
+import numpy as np
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from keras.utils.np_utils import to_categorical
+from keras.layers import  MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D
+from keras.models import Sequential
+
